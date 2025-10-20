4061e71fad6f15b5436ac41b4734169208f7d228
iaean <iaean@users.noreply.github.com>
2025-10-20 11:09:40 +0200

    Update CI

--- release.js
+++ release.js
@@ -4,23 +4,8 @@ module.exports = {
   dryRun: false,
   plugins: [
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
     [ "@semantic-release/npm", {
         npmPublish: false } ],
-    [ "@semantic-release/git", {
-        assets: [
-          "VERSION",
-          "openapi.yaml",
-          "CHANGELOG.md",
-          "package.json" ],
-        message: "Bumped new version ${nextRelease.version} [skip ci]" }
-    ],
-    // BuildKit is not able to push back multi arch builds via buildx build --load
-    // We cannot use the plugin for pushing multi arch
-    /* [ "@semantic-release-plus/docker", {
-        skipLogin: true,
-        name: "name/repo:semantic-release" }
-    ], */
     [ "@semantic-release/github", {
         assets: [
           { path: "openapi.yaml", label: "DNS3L OpenAPI (Swagger) Specification" } ],
